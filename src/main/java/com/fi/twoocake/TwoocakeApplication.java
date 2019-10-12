c0868bf99a9dd8fee0c2b572b309fe6eae3a3334
Van <jiamingvanhuang.work@outlook.com>
2019-10-13 02:16:06 +0800

    Initial commit

---
+++ src/main/java/com/fi/twoocake/TwoocakeApplication.java
@@ -0,0 +1,13 @@
+package com.fi.twoocake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoocakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwoocakeApplication.class, args);
+    }
+
+}
